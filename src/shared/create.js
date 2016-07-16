51146f939682dabbb9af9bde6c6b37475961ba02
Dan Bruder <dbruder@rapiddg.com>
2016-07-16 01:55:58 -0400

    Changes

--- src/shared/create.js
+++ src/shared/create.js
@@ -47,7 +47,7 @@ export default function create(providedMiddleware, data) {
   let store = {}
   if (__CLIENT__) {
     const enhancer = compose(
-      ...middleware,
+      applyMiddleware(...middleware),
       persistState(),
     )
 
